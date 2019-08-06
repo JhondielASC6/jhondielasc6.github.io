89f385434c643b0c1848262147b4e0fcaf5c987f
jhondielASC6 <jhondiel1234@gmail.com>
2019-08-06 15:48:16 -0400

    not done

--- mechanics.js
+++ mechanics.js
@@ -1,5 +1,19 @@
-function pageScroll() {
-    window.scrollBy(0,500);
-    scrolldelay = setTimeout('pageScroll()',100); 
-    return pageScroll
-}
+$(document).ready(function() {
+    $('html, body').animate({
+      scrollTop: $("#yadyad").offset().top
+    }, 1000);
+  
+    $('html, body').animate({
+      scrollTop: $("#yuri").offset().top
+    }, 2000);
+
+  });
+
+setTimeout( function newWindow( ){
+  $(window).scroll(function() {
+   if($(window).scrollTop() + $(window).height() > $(document).height() - 50) {
+    window.location.replace("Mypage/Mypage.html");
+  }
+});
+},1000)
+
